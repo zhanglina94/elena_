39c94d83a644a2b4dd351eb1b663664bb0a58251
LINA ZHANG <43246784+zhanglina94@users.noreply.github.com>
2022-11-29 00:01:47 +0800

    Create projects.js

---
+++ src/api/projects.js
@@ -0,0 +1,32 @@
+import Crwn from "../assets/portfolio/Crwn.jpg"
+import Tango from "../assets/portfolio/Tango.jpg"
+import Chumkin from "../assets/portfolio/lina.jpg"
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    img: Crwn,
+    title: "CRWN CLOTHING",
+    desc: "A React E-commerce Progressive Web App which has Authentication, Database, CRUD, Payment features.",
+    demo: "https://sgcr7crwn-live.herokuapp.com/",
+    source: "https://github.com/sagnikghoshcr7/crwn-clothing",
+  },
+  {
+    img: Tango,
+    title: "TANGO BROWSER",
+    desc: "Modern Browser App based on chromium & .NET Framework. Our Browser has all modern features that chrome has.",
+    demo: "https://github.com/MSAC-KIIT/TangoBrowser/blob/main/README.md",
+    source: "https://github.com/MSAC-KIIT/TangoBrowser",
+  },
+  {
+    img: lina,
+    title: "Lina",
+    desc: "A Flutter Instagram Clone which has Authentication, Database, Profile, Image Uploading with Live Geolocation, Like & Comment features.",
+    demo: "https://github.com/zhanglina94/zhanglina94.github.io#readme",
+    source: "https://github.com/zhanglina94",
+  },
+
+
+
+]
